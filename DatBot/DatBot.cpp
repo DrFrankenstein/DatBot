a032546645f02038cce48f50c998bce17be30a37
Carl Tessier <doktorfrankenstein@gmail.com>
2020-02-23 22:18:03 -0500

    fix: don't throw from main

--- DatBot/DatBot.cpp
+++ DatBot/DatBot.cpp
@@ -3,14 +3,16 @@
 
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
+#include <cstdint>
 #include <cstdlib>
+#include <exception>
 #include <iostream>
 #include <string>
 #include <yaml-cpp/yaml.h>
 
 namespace asio = boost::asio;
 namespace po   = boost::program_options;
-using std::cout, std::endl, std::exit, std::string;
+using std::cerr, std::cout, std::endl, std::exception, std::exit, std::string, std::uint16_t;
 using YAML::Node;
 
 po::variables_map parseOptions(int argc, char** argv)
@@ -39,16 +41,29 @@ po::variables_map parseOptions(int argc, char** argv)
 
 int main(int argc, char* argv[])
 {
-	constexpr ::uint16_t DEFAULT_PORT = 6667;
-	auto options = parseOptions(argc, argv);
+	try
+	{
+		constexpr uint16_t DEFAULT_PORT = 6667;
 
-	Node config = YAML::LoadFile(options["config-file"].as<string>());
-	std::cout << "config:" << config << std::endl;
+		auto options = parseOptions(argc, argv);
+		Node config = YAML::LoadFile(options["config-file"].as<string>());
+		std::cout << "config:" << config << std::endl;
 
-	asio::io_context io;
-	Net::AsioDevice tcpreader(io, "irc.snoonet.org", DEFAULT_PORT);
-	Bot::IrcBot bot(tcpreader, config);
+		asio::io_context io;
+		Net::AsioDevice tcpreader(io, "irc.snoonet.org", DEFAULT_PORT);
+		Bot::IrcBot bot(tcpreader, config);
 
-	bot.start();
-	io.run();
+		bot.start();
+		io.run();
+	}
+	catch (exception& ex)
+	{
+		cerr << "ERROR: " << ex.what() << endl;
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		cerr << "Unknown ERROR" << endl;
+		return EXIT_FAILURE;
+	}
 }
